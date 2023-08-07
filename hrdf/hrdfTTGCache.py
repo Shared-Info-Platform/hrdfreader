c4bc7acb74b0cc566858cd9410886d953d36659f
arneheimann <arne.heimann@bernmobil.com>
2023-08-07 15:20:18 +0200

    implementing lookup hack

--- hrdf/hrdfTTGCache.py
+++ hrdf/hrdfTTGCache.py
@@ -194,7 +194,7 @@ class HrdfTTGCache:
 						   "   AND b.tripno = a.tripno "\
 						   "   AND b.operationalno = a.operationalno "\
 						   "   AND b.bitfieldno = c.bitfieldno "\
-						   "   AND c.bitfieldarray && array%s::date[] "
+						   "   AND c.bitfieldarray && %s::date[] "
 		curGleis = self.__hrdfdb.connection.cursor()
 		curGleis.execute(sql_selGleisData, (eckdatenid, generationDatesArray,))
 		allGleise = curGleis.fetchall()
