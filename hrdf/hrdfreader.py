72476b0a65fe97b0a7268a2d9e343241bb27bde8
arneheimann <arne.heimann@bernmobil.com>
2023-08-18 08:29:25 +0200

    fix linie_strIOs

--- hrdf/hrdfreader.py
+++ hrdf/hrdfreader.py
@@ -474,7 +474,17 @@ class HrdfReader:
 				languagecode = line[1:3].lower()
 
 		for line in linienDict:
-			logger.debug('Add Linie to strIO')
+			logger.debug('Add Linie'+linienDict[line].get('line_key')+'to strIO')
+			logger.debug('Eckdaten: '+self.__fkdict['fk_eckdatenid']+', ')
+			logger.debug('line_key: '+linienDict[line].get('line_key')+', ')
+			logger.debug('number_intern: '+linienDict[line].get('number_intern', None)+', ')
+			logger.debug('name_short: '+linienDict[line].get('name_short', None)+', ')
+			logger.debug('name_short_index: '+linienDict[line].get('name_short_index', None)+', ')
+			logger.debug('name_long: '+linienDict[line].get('name_long', None)+', ')
+			logger.debug('name_long_index: '+linienDict[line].get('name_long_index', None)+', ')
+			logger.debug('color_font: '+linienDict[line].get('color_font', None)+', ')
+			logger.debug('color_back: '+linienDict[line].get('color_back', None)+', ')
+
 			linie_strIO.write(self.__fkdict['fk_eckdatenid']+';'
 									+linienDict[line].get('line_key')+';'
 									+linienDict[line].get('number_intern', None)+';'
