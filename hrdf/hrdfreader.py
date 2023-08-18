e6a8b96c3e62af3ebeb7beb99e84cfda4edb252a
arneheimann <arne.heimann@bernmobil.com>
2023-08-18 07:57:42 +0200

    fix linie_strIOs

--- hrdf/hrdfreader.py
+++ hrdf/hrdfreader.py
@@ -476,6 +476,7 @@ class HrdfReader:
 		for line in linienDict:
 			logger.debug('Add Linie to strIO')
 			logger.debug('Linie: '+linienDict[line].get('line_key','fail'))
+			logger.debug('Linie: '+line.get('line_key','fail'))
 			linie_strIO.write(self.__fkdict['fk_eckdatenid']+';'
 									+lineV.get('line_key')+';'
 									+lineV.get('number_intern', None)+';'
