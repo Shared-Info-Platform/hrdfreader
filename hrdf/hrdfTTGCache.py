c2077de3345f462a88f92a81888f03e39d427b08
arneheimann <arne.heimann@bernmobil.com>
2023-08-26 10:27:02 +0200

    erweiterte Linieninfos berücksichtigen

--- hrdf/hrdfTTGCache.py
+++ hrdf/hrdfTTGCache.py
@@ -278,6 +278,11 @@ class HrdfTTGCache:
 				LList = list()
 				LList.append(fahrtL)
 				self.__fahrtLinienLookup[fahrtL[5]] = LList
+				# test for debugging
+				if (fahrtL[5] == '79989686'):
+					logger.debug("Liste für 79989686 ist: {}".format(str(LList)))
+				if (fahrtL[5] == '80739098'):
+					logger.debug("Liste für 80739098 ist: {}".format(str(LList)))		
 		allLs.clear()
 
 		# Lookup für erweiterte Linieninformationen der Fahrten und merge in Linienlookup
